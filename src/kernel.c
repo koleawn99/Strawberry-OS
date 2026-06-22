a3e2e509c4110c80aabf2c28f434da1c8d020176
koleawn <koleawn99@gmail.com>
2026-06-22 23:39:29 +0300

    style: switch to a clean black background with pink text terminal

--- src/kernel.c
+++ src/kernel.c
@@ -1,31 +1,18 @@
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 25
 
-#define COLOR_DARK_GRAY 8
-#define COLOR_LIGHT_PINK 13
-
 //Variables to track terminal state
 int terminal_row = 0;
 int terminal_column = 0;
+
+unsigned short attribute;
 volatile unsigned short* video_memory = (volatile unsigned short*)0xB8000;
-unsigned short color_atribute;
 
-//Clear the screen
-void terminal_initalize(void) 
-{
-    unsigned short color_attribute = ((COLOR_DARK_GRAY << 4) | COLOR_LIGHT_PINK) << 8;
- 
-    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
-        video_memory[i] = ' ' | color_attribute;
-    }
-
-    terminal_row = 0;
-    terminal_column = 0;
-    
-}
 
 //Write a single raw character onto the grid
 void termianl_putchar(char c){
+
+        attribute = ((0 << 4) | 13) << 8;
 
     //Handle the newline escape character
     if (c == '\n'){
@@ -36,7 +23,7 @@ void termianl_putchar(char c){
     
     //Map out the 1D absolute index from 2D coordinates
     int index = terminal_row * SCREEN_WIDTH + terminal_column;
-    video_memory[index] = c | color_atribute;
+    video_memory[index] = c | attribute;
 
     //Move the cursor right
     terminal_column++;
@@ -48,6 +35,21 @@ void termianl_putchar(char c){
         terminal_row++; 
     }
 }
+
+//Clear the screen
+void terminal_initalize(void) 
+{
+    
+ 
+    for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
+        video_memory[i] = ' ' | attribute;
+    }
+
+    terminal_row = 0;
+    terminal_column = 0;
+    
+}
+
 
 //Loop throgh an entire string array until the null terminator
 void terminal_write_string(const char* data){
@@ -66,5 +68,5 @@ void kernel_main(void){
     terminal_write_string("Welcome to Strawberry OS!\n");
     terminal_write_string("-------------------------\n");
     terminal_write_string("The core print engine is alive.\n");
-    terminal_write_string("Successfully tracking cursor and procesing new lines!\n");
+    terminal_write_string("Strawberry OS is booting up! Let's check how the lines wrap when the text hits eighty characters.\n");
 }
