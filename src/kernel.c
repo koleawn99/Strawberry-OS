7bb58adcc4fcf27ec20802747f3279a01de8be79
koleawn <koleawn99@gmail.com>
2026-06-22 23:14:27 +0300

    feat:implemented the terminal tracking engine and created the print function

--- src/kernel.c
+++ src/kernel.c
@@ -4,23 +4,67 @@
 #define COLOR_DARK_GRAY 8
 #define COLOR_LIGHT_PINK 13
 
-void kernel_main(void) 
-{
-    volatile unsigned short* video_memory = (volatile unsigned short*)0xB8000;
-    unsigned short color_attribute = ((COLOR_DARK_GRAY << 4) | COLOR_LIGHT_PINK) << 8;
+//Variables to track terminal state
+int terminal_row = 0;
+int terminal_column = 0;
+volatile unsigned short* video_memory = (volatile unsigned short*)0xB8000;
+unsigned short color_atribute;
 
-    // 1. Clear the screen
+//Clear the screen
+void terminal_initalize(void) 
+{
+    unsigned short color_attribute = ((COLOR_DARK_GRAY << 4) | COLOR_LIGHT_PINK) << 8;
+ 
     for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; i++) {
         video_memory[i] = ' ' | color_attribute;
     }
 
-    // 2. Define our welcome string as a static constant pointer
-    static const char* message = "Welcome to Strawberry OS!";
+    terminal_row = 0;
+    terminal_column = 0;
+    
+}
 
-    // 3. Print the string safely by pulling directly from physical memory
+//Write a single raw character onto the grid
+void termianl_putchar(char c){
+
+    //Handle the newline escape character
+    if (c == '\n'){
+        terminal_column = 0;
+        terminal_row++;
+        return;
+    }
+    
+    //Map out the 1D absolute index from 2D coordinates
+    int index = terminal_row * SCREEN_WIDTH + terminal_column;
+    video_memory[index] = c | color_atribute;
+
+    //Move the cursor right
+    terminal_column++;
+
+    //Move to the next line if we hit the edge of the screen
+    if (terminal_column >= SCREEN_WIDTH){
+
+        terminal_column = 0;
+        terminal_row++; 
+    }
+}
+
+//Loop throgh an entire string array until the null terminator
+void terminal_write_string(const char* data){
     int i = 0;
-    while (message[i] != '\0') {
-        video_memory[i] = message[i] | color_attribute;
+    while(data[i] != '\0'){
+        termianl_putchar(data[i]);
         i++;
     }
+}
+
+void kernel_main(void){
+    //Initialize the clean canvas
+    terminal_initalize();
+
+    //Using the new print function
+    terminal_write_string("Welcome to Strawberry OS!\n");
+    terminal_write_string("-------------------------\n");
+    terminal_write_string("The core print engine is alive.\n");
+    terminal_write_string("Successfully tracking cursor and procesing new lines!\n");
 }
